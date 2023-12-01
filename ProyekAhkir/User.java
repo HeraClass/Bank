2c455d45b574b853742727a79cca8eef527993d6
Denny Mahendra Satria Putra <71745488+HeraClass@users.noreply.github.com>
2023-12-01 16:37:38 +0700

    first-commit

--- ProyekAhkir/User.java
+++ ProyekAhkir/User.java
@@ -5,14 +5,12 @@ public class User {
     private String password;
     private String name;
 
-    // Existing constructor
     public User(String username, String password, String name) {
         this.username = username;
         this.password = password;
         this.name = name;
     }
 
-    // New constructor
     public User(String username, String password) {
         this.username = username;
         this.password = password;
@@ -28,5 +26,4 @@ public class User {
         return name;
     }
 
-    // Getter and setter methods for 'password' and 'name' if needed
 }
